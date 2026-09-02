c460d94bb6eab0b7576d83233f40594805d272ea
jware13 <jacob.ware09@gmail.com>
2026-09-02 14:04:38 -0400

    .

--- FinalProject/api/models/reviews.py
+++ FinalProject/api/models/reviews.py
@@ -8,11 +8,11 @@ class Review(Base):
     __tablename__ = "reviews"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    customer_name = Column(String, nullable=False)
+    customer_name = Column(String(120), nullable=False)
     menu_item_id = Column(Integer, ForeignKey("menu_items.id"), nullable=False)
     rating = Column(Integer, nullable=False)
     created_at = Column(DateTime, nullable=False, server_default=func.now())
-    comment = Column(String, nullable=True)
+    comment = Column(String(1000), nullable=True)
     customer_id = Column(Integer, ForeignKey("customers.id"), nullable=True)
 
     #customer = relationship("Customer", back_populates="reviews")
